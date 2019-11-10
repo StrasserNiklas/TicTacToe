d6241de542f9805a49dbc1e2dc97c664831cd8cd
StrasserNiklas <niklasstrasser@hotmail.com>
2019-11-10 18:26:21 +0100

    Client test aufbau ohne TicTacToe logik, die mach ich morgen

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Client.Models;
 
 namespace Client
 {
@@ -23,6 +27,16 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddHttpClient("TicTacToeGame", client =>
+                    {
+                        client.BaseAddress = new Uri("https://localhost:5001");
+                    }).AddTypedClient<GameClientService>();
+
+                }).Build();
         }
     }
 }
---
+++ Client/Models/GameClientService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class GameClientService
+    {
+        private readonly HttpClient _httpClient;
+        //private readonly ILogger _logger;
+        public GameClientService(HttpClient httpClient)//, ILogger<BooksClientService> logger)
+        {
+            _httpClient = httpClient;
+            //_logger = logger;
+        }
+
+        public async Task<IEnumerable<Player>> GetBooksAsync()
+        {
+            //_logger.LogDebug("GetBooksAsync called");
+            var response = await _httpClient.GetAsync("/api/Books");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var books = JsonConvert.DeserializeObject<IEnumerable<Player>>(json);
+            return books;
+        }
+
+        public async Task<Player> AddBookAsync(Player book)
+        {
+            string json = JsonConvert.SerializeObject(book);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("/api/Books", content);
+            response.EnsureSuccessStatusCode();
+            var returnJson = await response.Content.ReadAsStringAsync();
+            var updatedBook = JsonConvert.DeserializeObject<Player>(returnJson);
+            return updatedBook;
+        }
+    }
+}
