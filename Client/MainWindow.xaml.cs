1ef925b9bbca703f7a55c54e2487d01e27bae2ca
StrasserNiklas <niklasstrasser@hotmail.com>
2019-11-11 12:17:18 +0100

    Xaml listview ohne header

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -63,6 +63,8 @@ namespace Client
             {
                 var list = await this.gameService.GetPlayerListAsync();
 
+
+                // hier würd daweil eine exception kommen
                 //var playerList = await this.gameService.PostPlayerInfoToServerAsync("Nikolaus");
 
                 
