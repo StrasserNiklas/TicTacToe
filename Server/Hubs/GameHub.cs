195689a5ef00ba75503f666287188af8a2194cf8
Yannick Gruber <yannick.gruber@fhwn.ac.at>
2019-12-17 11:37:45 +0100

    GameHub function comment stylecop

--- Server/Hubs/GameHub.cs
+++ Server/Hubs/GameHub.cs
@@ -1,9 +1,9 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="ActionVisitor.cs" company="FHWN">
+// <copyright file="GameHub.cs" company="FHWN">
 // Copyright (c) FHWN. All rights reserved.
 // </copyright>
 // <author>Felix Brandstetter, Niklas Strasser, Yannick Gruber</author>
-// <summary>This file represents </summary>
+// <summary>This file represents the SignalR Hub for the TicTacToe server.</summary>
 //-----------------------------------------------------------------------
 
 namespace Server.Hubs
@@ -17,18 +17,33 @@ namespace Server.Hubs
     using System.Linq;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// The SignalR Hub for the Tic-Tac-Toe server.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.SignalR.Hub" />
     public class GameHub : Hub
     {
+        /// <summary>
+        /// This field is used to save the main service.
+        /// </summary>
         private readonly IMainService mainService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameHub"/> class.
+        /// </summary>
+        /// <param name="main">The main service.</param>
         public GameHub(IMainService main)
         {
             this.mainService = main;
         }
 
+        /// <summary>
+        /// Gets the players from the MainService and sends them to all clients.
+        /// </summary>
+        /// <param name="requestedPlayerName">Name of the requested player.</param>
         public async Task GetPlayers(string requestedPlayerName)
         {
-            var allPlayers = await mainService.GetPlayersAsync();
+            var allPlayers = await this.mainService.GetPlayersAsync();
 
             // select all players except the requested one
             // requested player should not be included in the result
@@ -37,6 +52,10 @@ namespace Server.Hubs
             await base.Clients.All.SendAsync("ReceivePlayersAsync", await this.mainService.GetPlayersNotInGameAsync());
         }
 
+        /// <summary>
+        /// Adds a new player to the list of players, send all players to the clients, send all games and the new player instance to the caller.
+        /// </summary>
+        /// <param name="nameForNewPlayer">The name for the new player.</param>
         public async Task AddPlayer(string nameForNewPlayer)
         {
             Player newPlayer = new Player(nameForNewPlayer) { ConnectionId = Context.ConnectionId };
@@ -67,6 +86,10 @@ namespace Server.Hubs
             await base.Clients.All.SendAsync("ReceivePlayersAsync", await this.mainService.GetPlayersNotInGameAsync());
         }
 
+        /// <summary>
+        /// Adds the new game request to the list of game requests and send the request to the desired player.
+        /// </summary>
+        /// <param name="gameRequest">The game request.</param>
         public async Task AddGameRequest(GameRequest gameRequest)
         {
             var list = await this.mainService.GetPlayersAsync();
@@ -106,6 +129,11 @@ namespace Server.Hubs
             }
         }
 
+        /// <summary>
+        /// Declines or accepts the game request and sends a decline message or various game data to different clients.
+        /// </summary>
+        /// <param name="id">The game request identifier.</param>
+        /// <param name="accept">if set to <c>true</c> [accept].</param>
         public async Task DeclineOrAcceptRequest(int id, bool accept)
         {
             var requests = new List<GameRequest>(await this.mainService.GetGameRequestsAsync());
@@ -142,6 +170,10 @@ namespace Server.Hubs
             }
         }
 
+        /// <summary>
+        /// Updates the game status after a move.
+        /// </summary>
+        /// <param name="update">The GameStatus to update.</param>
         public async Task UpdateGameStatus(GameStatus update)
         {
             var games = new List<Game>(await this.mainService.GetGamesAsync());
@@ -160,6 +192,11 @@ namespace Server.Hubs
             }
         }
 
+        /// <summary>
+        /// Removes the game from the list, send update to the enemy and send game and player list to all clients.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="enemyId">The enemy identifier.</param>
         public async Task ReturnToLobby(string id, string enemyId)
         {
             var games = new List<Game>(await this.mainService.GetGamesAsync());
@@ -181,6 +218,12 @@ namespace Server.Hubs
             }
         }
 
+        /// <summary>
+        /// Updates the game after a move and sends the game status to concerned clients.
+        /// </summary>
+        /// <param name="game">The game.</param>
+        /// <param name="updatedPosition">The updated position.</param>
+        /// <param name="player">The player.</param>
         private async Task UpdatePlayerSpecificGameStatus(Game game, int updatedPosition, Player player)
         {
             if (game.IsMoveValid(updatedPosition, player))
@@ -209,6 +252,13 @@ namespace Server.Hubs
             }
         }
 
+        /// <summary>
+        /// Creates a new game status.
+        /// </summary>
+        /// <param name="game">The game instance.</param>
+        /// <param name="isNewGame">if set to <c>true</c> [is new game].</param>
+        /// <param name="updatedPosition">The updated position in the field.</param>
+        /// <returns></returns>
         private GameStatus CreateNewGameStatus(Game game, bool isNewGame, int updatedPosition = -1)
         {
             var gameStatus = new GameStatus(game.CurrentGameStatus, game.CurrentPlayer.ConnectionId, game.CurrentPlayer.Marker, game.GameId, game.PlayerOne.Wins, game.PlayerTwo.Wins)
@@ -224,11 +274,21 @@ namespace Server.Hubs
             return gameStatus;
         }
 
+        /// <summary>
+        /// Called when a new connection is established with the hub.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous connect.
+        /// </returns>
         public override Task OnConnectedAsync()
         {
             return base.OnConnectedAsync();
         }
 
+        /// <summary>
+        /// Called when a connection with the hub is terminated, removes the player and sends the list to all clients.
+        /// </summary>
+        /// <param name="exception"></param>
         public async override Task OnDisconnectedAsync(Exception exception)
         {
             var id = Context.ConnectionId;
