546dc229079a409956f96045f3b1f37c60355e87
StrasserNiklas <niklasstrasser@hotmail.com>
2019-12-17 15:34:37 +0100

    Tests, stylecop 0 errors

--- Tests/TicTacToeTests.cs
+++ Tests/TicTacToeTests.cs
@@ -9,9 +9,14 @@
 namespace Tests
 {
     using System;
+    using System.Linq;
+    using System.Threading.Tasks;
     using GameLibrary;
+    using Microsoft.Extensions.Logging;
+    using Moq;
     using NUnit.Framework;
     using Server.Models;
+    using Server.Services;
 
     /// <summary>
     /// Represents the Unit Tests for our solution.
@@ -38,7 +43,7 @@ namespace Tests
 
             Game game = new Game(player1, player2);
             game.NewGameSetup();
-            
+
             var finishedAfterMove1 = game.MakeMove(0, player1);
             var finishedAfterMove2 = game.MakeMove(4, player2);
             var finishedAfterMove3 = game.MakeMove(8, player1);
@@ -95,5 +100,262 @@ namespace Tests
             Assert.AreEqual(0, player1.Wins);
             Assert.AreEqual(0, player2.Wins);
         }
-    }
+
+        /// <summary>
+        /// Tests the service add player method.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Add_Player()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+            var player = new Player();
+
+            var playerReturned = await service.AddPlayerAsync(player);
+
+            Assert.AreEqual(player, playerReturned);
+            var list = service.GetPlayersNotInGameAsync().Result.ToList();
+            Assert.Contains(playerReturned, list);
+        }
+
+        /// <summary>
+        /// Tests the service add game request.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Add_Request()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player("Player1");
+            Player player2 = new Player("Player2");
+
+            GameRequest request = new GameRequest(player1, player2);
+            await service.AddGameRequestAsync(request);
+
+            var requestList = service.GetGameRequestsAsync().Result.ToList();
+            Assert.Contains(request, requestList);
+        }
+
+        /// <summary>
+        /// Tests the get game requests method.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Get_Requests()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player("Felix");
+            Player player2 = new Player("Niklas");
+            Player player3 = new Player("Yannick");
+            Player player4 = new Player("Emanuel");
+
+            GameRequest request1 = new GameRequest(player1, player2);
+            GameRequest request2 = new GameRequest(player3, player4);
+            await service.AddGameRequestAsync(request1);
+            await service.AddGameRequestAsync(request2);
+
+            var requestList = service.GetGameRequestsAsync().Result.ToList();
+            Assert.Contains(request1, requestList);
+            Assert.Contains(request2, requestList);
+            Assert.AreEqual(2, requestList.Count);
+        }
+
+        /// <summary>
+        /// Tests the service remove request.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Remove_Request()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player("Player1");
+            Player player2 = new Player("Player2");
+            var request = new GameRequest(player1, player2);
+            var addedRequest = await service.AddGameRequestAsync(request);
+
+            var removedRequest = await service.RemoveRequestAsync(addedRequest, false);
+
+            Assert.AreEqual(addedRequest, removedRequest);
+        }
+
+        /// <summary>
+        /// Tests the service get players not in game. 
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Get_Players_Not_In_Game()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Player player3 = new Player();
+
+            player1.ConnectionId = "1";
+            player2.ConnectionId = "2";
+            player3.ConnectionId = "3";
+
+            await service.AddPlayerAsync(player1);
+            await service.AddPlayerAsync(player2);
+            await service.AddPlayerAsync(player3);
+
+            Game game1 = new Game(player1, player2);
+            await service.AddGameAsync(game1);
+
+            var playersNotInGame = service.GetPlayersNotInGameAsync().Result.ToList();
+            Assert.AreEqual(1, playersNotInGame.Count);
+
+            Assert.IsTrue(playersNotInGame.Contains(player3));
+        }
+
+        /// <summary>
+        /// Tests the service get games method.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Get_Games()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            Game game1 = new Game(player1, player2);
+            Game game2 = new Game(player1, player2);
+
+            await service.AddGameAsync(game1);
+            await service.AddGameAsync(game2);
+
+            var gameList = service.GetGamesAsync().Result.ToList();
+            Assert.AreEqual(2, gameList.Count);
+        }
+
+        /// <summary>
+        /// Tests the service get simple game information list.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Get_Simple_Game_Information_List()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player("Yannicka");
+            Player player2 = new Player("Felarius");
+            Game game1 = new Game(player1, player2);
+            await service.AddGameAsync(game1);
+
+            var simpleGameInfo = service.GetSimpleGameInformationListAsync().Result;
+
+            Assert.That(() => simpleGameInfo.First().PlayerOne == player1.PlayerName);
+            Assert.That(() => simpleGameInfo.First().PlayerTwo == player2.PlayerName);
+        }
+
+        /// <summary>
+        /// Tests the service remove game.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Remove_Game()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            Game game1 = new Game(player1, player2);
+            Game game2 = new Game(player1, player2);
+
+            await service.AddGameAsync(game1);
+            await service.AddGameAsync(game2);
+
+            var gameList = service.GetGamesAsync().Result.ToList();
+            Assert.AreEqual(2, gameList.Count);
+
+            await service.RemoveGameAsync(game2);
+            gameList = service.GetGamesAsync().Result.ToList();
+
+            Assert.IsFalse(gameList.Contains(game2));
+        }
+
+        /// <summary>
+        /// Tests the service remove player.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Remove_Player()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            await service.AddPlayerAsync(player1);
+            await service.AddPlayerAsync(player2);
+
+            var playerList = service.GetPlayersAsync().Result.ToList();
+            Assert.AreEqual(2, playerList.Count);
+
+            await service.RemovePlayerAsync(player2);
+            playerList = service.GetPlayersAsync().Result.ToList();
+
+            Assert.IsFalse(playerList.Contains(player2));
+        }
+
+        /// <summary>
+        /// Tests the service add game.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Add_Game()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            Game game1 = new Game(player1, player2);
+            var game = await service.AddGameAsync(game1);
+
+            var gameList = service.GetGamesAsync().Result.ToList();
+            Assert.Contains(game1, gameList);
+        }
+
+        /// <summary>
+        /// Tests the service get players.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous method.</returns>
+        [Test]
+        public async Task Test_Service_Get_Players()
+        {
+            var mock = new Mock<ILogger<MainService>>();
+            var service = new MainService(mock.Object);
+            var player1 = new Player("Felix");
+            var player2 = new Player("Niklas");
+            var player3 = new Player("Yannick");
+
+            var player1Returned = await service.AddPlayerAsync(player1);
+            var player2Returned = await service.AddPlayerAsync(player2);
+            var player3Returned = await service.AddPlayerAsync(player3);
+
+            var players = service.GetPlayersAsync().Result.ToList();
+
+            Assert.Contains(player1Returned, players);
+            Assert.Contains(player2Returned, players);
+            Assert.Contains(player3Returned, players);
+            Assert.AreEqual(3, players.Count);
+        }
+    }   
 }
