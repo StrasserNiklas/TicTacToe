0742caa440180327b1b8579499a6ad9bed0120b4
Felix Brandstetter <felix.brandstetter@fhwn.ac.at>
2021-02-03 11:26:34 +0100

    added DB Manager

---
+++ Server/DBManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class DBManager
+    {
+        public void AddNewPlayer(string username, string passwordHash)
+        {
+            // INSERT INTO Users (username, password) VALUES (..., ...)
+        }
+
+        public void CheckLogin(string username, string passwordHash)
+        {
+
+        }
+
+        public void UpdatePlayerWins()
+        {
+
+        }
+
+        public void GetLeaderBoardData()
+        {
+
+        }
+    }
+}
--- TicDatabse/Users.sql
+++ TicDatabse/Users.sql
@@ -1,4 +1,6 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] NCHAR(10) NOT NULL, 
+    [password] NCHAR(10) NOT NULL
 )
